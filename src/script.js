71a36c48b79ace214a618c607ef487c476ac1065
Viren Hirpara <62017836+viru185@users.noreply.github.com>
2025-11-04 02:08:04 +0530

    - fixed form submition

--- src/script.js
+++ src/script.js
@@ -340,6 +340,7 @@ const initContactForm = () => {
                 method: (contactForm.method || "POST").toUpperCase(),
                 body: new FormData(contactForm),
                 headers: { Accept: "application/json" },
+                
             });
 
             if (!response.ok) {
